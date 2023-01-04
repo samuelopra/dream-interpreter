ba3eeac203d50ab4fca59c83c1b6868dab713c60
sokpara <sokpara@adobe.com>
2023-01-04 13:12:05 -0500

    fix bug where gpt3 completes user input lol

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,7 +17,7 @@ const generateAction = async (req, res) => {
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${basePromptPrefix}${req.body.userInput}.`,
     temperature: 0.8,
     max_tokens: 500,
   });
