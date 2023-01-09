ba9d32d24c7eb16b761dc4b58b4d707e11c95c72
Nazim Amin <amin.nazim13@gmail.com>
2023-01-09 18:05:53 -0500

    Add initial DALE 2 prompt

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
     setIsGenerating(true);
 
     console.log('Calling OpenAI...');
-    const response = await fetch('/api/generate', {
+    const response = await fetch('/api/generate/image', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
